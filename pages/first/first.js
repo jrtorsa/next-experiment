f32bd93368f58add9ad3ee4e71bf9b1611e3ec3d
Raul T <jr.torsa@gmail.com>
2020-10-06 11:22:44 -0600

    adding library

--- pages/first/first.js
+++ pages/first/first.js
@@ -1,19 +1,33 @@
-import Link from 'next/link'
+import Link from "next/link";
 
+export default function First() {
+  return (
+    <>
+      <h1>First</h1>
 
+      <Link href="/">
+        <a>App</a>
+      </Link>
+      <br />
+      <Link href="/first/second/second">
+        <a>Second</a>
+      </Link>
+      <br />
+      <Link href="/first/second/third/third">
+        <a>Third</a>
+      </Link>
+    </>
+  );
+}
 
-export default function First(){
-    return(
-        <>
-            <h1>First</h1>
+export async function getServerSideProps() {
+  const url = "https://graphqlzero.almansi.me/api";
+  const res = await fetch(url);
+  const data = await res.json();
 
-            <Link href='/'><a>App</a></Link>
-            <br />
-            <Link href='/first/second/second'><a>Second</a></Link>
-            <br />
-            <Link href='/first/second/third/third'><a>Third</a></Link>
+  console.log(data);
 
-           
-        </>
-    )
+  return {
+    props: {},
+  };
 }
